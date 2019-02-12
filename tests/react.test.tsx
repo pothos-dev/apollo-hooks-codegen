7a1e9dce4cc1b229dd24cbe478dbe66db87099f0
Daniel Ebert <15155173+bearbytes@users.noreply.github.com>
2019-02-12 19:42:37 +0100

    fakerql tests

--- tests/react.test.tsx
+++ tests/react.test.tsx
@@ -2,14 +2,45 @@ import 'jsdom-global/register'
 import React from 'react'
 import enzyme from 'enzyme'
 import EnzymeAdapter from 'enzyme-adapter-react-16'
+import {
+  ApolloHooksProvider,
+  useApolloQuery,
+  fetchProducts,
+} from './resources/fakerql-output'
+import ApolloClient from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import fetch from 'unfetch'
 
 enzyme.configure({ adapter: new EnzymeAdapter() })
 
+const apolloClient = new ApolloClient({
+  link: new HttpLink({
+    uri: 'https://fakerql.com/graphql',
+    credentials: 'include',
+    fetch,
+  }),
+  cache: new InMemoryCache(),
+})
+
 function TestComponent() {
-  return <div>Hello world</div>
+  const [data, error] = useApolloQuery(fetchProducts())
+
+  if (error) throw error
+  if (!data) return <div>Loading</div>
+
+  return <div>{data.allProducts.length}</div>
+}
+
+function TestApp() {
+  return (
+    <ApolloHooksProvider apolloClient={apolloClient}>
+      <TestComponent />
+    </ApolloHooksProvider>
+  )
 }
 
 test('react test', () => {
-  const wrapper = enzyme.mount(<TestComponent />)
+  const wrapper = enzyme.mount(<TestApp />)
   expect(wrapper.html()).toBe('<div>Hello world</div>')
 })
