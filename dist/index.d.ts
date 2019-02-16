dee15aeff81e104f95d0ea4762adc8f4728057eb
Daniel Ebert <15155173+bearbytes@users.noreply.github.com>
2019-02-16 13:03:06 +0100

    remove dist folder from repository

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { DocumentFile, GraphQLSchema } from 'graphql-codegen-core';
-export interface PluginConfig {
-    idType?: string;
-    scalarTypes?: {
-        [scalarName: string]: string;
-    };
-}
-export declare function plugin(schema: GraphQLSchema, documents: DocumentFile[], config: PluginConfig): string;
