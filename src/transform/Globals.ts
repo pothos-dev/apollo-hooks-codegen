0ae5ee4d64f44a728fea64e5d6dcdd33a66cf390
Daniel Ebert <15155173+bearbytes@users.noreply.github.com>
2019-03-20 22:19:48 +0100

    reorder

--- src/transform/Globals.ts
+++ src/transform/Globals.ts
@@ -8,13 +8,11 @@ let _inputTypes: TypeIR[]
 export function Schema() {
   return _schema
 }
-
-export function InputTypes() {
-  return _inputTypes
-}
-
 export function Config() {
   return _config
+}
+export function InputTypes() {
+  return _inputTypes
 }
 
 export function setGlobals(schema: GraphQLSchema, config: PluginConfig) {
